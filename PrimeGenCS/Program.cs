a439a54146808e4e71fc600a5d280f048f4eed1b
jfolken <jfolken@JFOLKENSSB10E>
2017-12-29 22:50:12 -0500

    prime number generator

---
+++ PrimeGenCS/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PrimeGenCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var primes = new List<ulong>();
+            
+            ulong iterator = 2;
+            
+            primes.Add(iterator);
+            Console.WriteLine(iterator);
+
+            var sw = new Stopwatch();
+
+            sw.Start();
+            while (true)
+            {
+                iterator += 1;
+                bool isPrime = true;
+
+                foreach (ulong prime in primes)
+                {
+                    if (iterator % prime == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    primes.Add(iterator);
+                    var primeCount = primes.LongCount();
+                    var elapsedSeconds = sw.ElapsedMilliseconds / 1000;
+                    var primesPerCount = elapsedSeconds == 0 ? primeCount : primeCount / elapsedSeconds;
+                    Console.WriteLine(iterator + "\t" + primeCount + "\t" + elapsedSeconds + "\t" + primesPerCount);
+                }
+
+
+
+
+            }
+            sw.Stop();
+
+        }
+    }
+}
