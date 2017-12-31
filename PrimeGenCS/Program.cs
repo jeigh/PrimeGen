79b2fe7343baf54e654e41081d966f628723936e
Jason <jason@home.folkens.org>
2017-12-31 00:29:14 -0500

    added some comments

--- PrimeGenCS/Program.cs
+++ PrimeGenCS/Program.cs
@@ -27,10 +27,17 @@ namespace PrimeGenCS
             while (true)
             {
                 iterator += 1;
+                
+                //todo: it might be more efficient to calculate the square root differently...  Possibly keep calculating square roots until the next integral square root is reached.
+                var squareRoot = Math.Sqrt(iterator);
                 bool isPrime = true;
 
+                //todo: instead of using primes here, use a truncated list of primes that's only expanded to a max value of the subject integer's square root.
                 foreach (ulong prime in primes)
                 {
+                    if (prime > squareRoot)
+                        break;
+
                     if (iterator % prime == 0)
                     {
                         isPrime = false;
@@ -40,16 +47,13 @@ namespace PrimeGenCS
 
                 if (isPrime)
                 {
+                    //todo: log the number into a database
                     primes.Add(iterator);
                     var primeCount = primes.LongCount();
                     var elapsedSeconds = sw.ElapsedMilliseconds / 1000;
                     var primesPerCount = elapsedSeconds == 0 ? primeCount : primeCount / elapsedSeconds;
                     Console.WriteLine(iterator + "\t" + primeCount + "\t" + elapsedSeconds + "\t" + primesPerCount);
                 }
-
-
-
-
             }
             sw.Stop();
 
