213f4ae52701adb07e6c26ef331c7388cd57710f
Jason Folkens <j-folkens@hotmail.com>
2022-02-18 17:12:52 -0500

    Fixed Output

--- PrimeGenCS/Program.cs
+++ PrimeGenCS/Program.cs
@@ -14,22 +14,22 @@ namespace PrimeGenCS
     {
         static void Main(string[] args)
         {
-            var primes = new List<ulong>();
+            List<ulong> primes = new List<ulong>();
             
             ulong iterator = 2;
             
             primes.Add(iterator);
             Console.WriteLine(iterator);
 
-            var sw = new Stopwatch();
+            Stopwatch sw = new Stopwatch();
 
             sw.Start();
             while (true)
             {
                 iterator += 1;
-                
+
                 //todo: it might be more efficient to calculate the square root differently...  Possibly keep calculating square roots until the next integral square root is reached.
-                var squareRoot = Math.Sqrt(iterator);
+                double squareRoot = Math.Sqrt(iterator);
                 bool isPrime = true;
 
                 //todo: instead of using primes here, use a truncated list of primes that's only expanded to a max value of the subject integer's square root.
@@ -49,10 +49,10 @@ namespace PrimeGenCS
                 {
                     //todo: log the number into a database
                     primes.Add(iterator);
-                    var primeCount = primes.LongCount();
-                    var elapsedSeconds = sw.ElapsedMilliseconds / 1000;
-                    var primesPerCount = elapsedSeconds == 0 ? primeCount : primeCount / elapsedSeconds;
-                    Console.WriteLine(iterator + "\t" + primeCount + "\t" + elapsedSeconds + "\t" + primesPerCount);
+                    long primeCount = primes.LongCount();
+                    long elapsedSeconds = sw.ElapsedMilliseconds / 1000;
+                    long primesPerCount = elapsedSeconds == 0 ? primeCount : primeCount / elapsedSeconds;
+                    Console.WriteLine($"Prime Found: {iterator},\tTotal Primes Found: {primeCount},\tSeconds Elapsed: {elapsedSeconds},\tPrimes per Second: {primesPerCount}");
                 }
             }
             sw.Stop();
